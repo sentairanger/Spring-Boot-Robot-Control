fa9fbd599cc47a8ac583002b3c8c8ce6bbc4a005
sentairanger <ed2point0@gmail.com>
2022-09-27 23:13:43 -0600

    Add files via upload

---
+++ torvalds/src/main/java/com/robot/torvalds/TorvaldsController.java
@@ -0,0 +1,54 @@
+package com.robot.torvalds;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.diozero.devices.motor.CamJamKitDualMotor;
+import com.diozero.util.SleepUtil;
+@Controller
+public class TorvaldsController {
+    @RequestMapping("/home")
+    public String getHome() {
+        return "home";
+    }
+
+    @RequestMapping("/forward")
+    public String goForward() {
+    try(CamJamKitDualMotor torvalds = new CamJamKitDualMotor()) {
+        torvalds.rotateRight(1);
+        SleepUtil.sleepSeconds(1);
+        torvalds.stop();
+        SleepUtil.sleepSeconds(1);
+    }
+        return "home";
+    }
+    @RequestMapping("/backward")
+    public String goBackward() {
+        try(CamJamKitDualMotor torvalds = new CamJamKitDualMotor()) {
+            torvalds.rotateLeft(1);
+            SleepUtil.sleepSeconds(1);
+            torvalds.stop();
+            SleepUtil.sleepSeconds(1);
+        }
+        return "home";
+    }
+    @RequestMapping("/left")
+    public String goLeft() {
+        try(CamJamKitDualMotor torvalds = new CamJamKitDualMotor()) {
+            torvalds.forward(1);
+            SleepUtil.sleepSeconds(0.3);
+            torvalds.stop();
+            SleepUtil.sleepSeconds(1);
+        }
+        return "home";
+    }
+    @RequestMapping("/right")
+    public String goRight() {
+        try(CamJamKitDualMotor torvalds = new CamJamKitDualMotor()) {
+            torvalds.backward(1);
+            SleepUtil.sleepSeconds(0.3);
+            torvalds.stop();
+            SleepUtil.sleepSeconds(1);
+        }
+        return "home";
+    }
+}
---
+++ torvalds/target/classes/static/logic.js
@@ -0,0 +1,12 @@
+$('#forward').on('mousedown', function(){
+	$.get('/forward');
+	});
+$('#backward').on('mousedown', function(){
+	$.get('/backward');
+	});
+$('#left').on('mousedown', function(){
+	$.get('/left');
+	});
+$('#right').on('mousedown', function(){
+	$.get('/right');
+	});
